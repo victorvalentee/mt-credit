45828fa00877348bca0f2c0d5c12591e6c7744ed
victorvalentee <victorvalentee@gmail.com>
2023-08-29 18:39:45 -0300

    Use strict data types and check holder_name length
    Signed-off-by: victorvalentee <victorvalentee@gmail.com>

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,26 @@
-CREATE TABLE IF NOT EXISTS cards (
-    exp_date VARCHAR NOT NULL,
-    holder_name VARCHAR NOT NULL,
-    card_number VARCHAR NOT NULL,
+DROP TABLE IF EXISTS cards;
+CREATE TABLE cards (
+    exp_date TEXT NOT NULL,
+    holder_name TEXT NOT NULL,
+    card_number TEXT NOT NULL,
     cvv INTEGER NOT NULL,
     PRIMARY KEY(holder_name, card_number, cvv)
-);
+) STRICT;
+
+-- As mentioned in the README file, data validation is going to be 
+-- implemented as database constraints (as much as possible).
+DROP TRIGGER IF EXISTS data_validation;
+
+CREATE TRIGGER data_validation
+BEFORE insert ON cards
+FOR EACH ROW
+BEGIN
+    SELECT RAISE(ABORT, 'Account holder name must have at least 3 characters.')
+    WHERE LENGTH(NEW.holder_name) < 3;
+END;
+
+CREATE TRIGGER block_updates
+BEFORE UPDATE ON cards
+BEGIN
+    SELECT RAISE(ABORT, "Updates not allowed. A new card must be issued.");
+END;
