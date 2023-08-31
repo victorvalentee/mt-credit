12bbde6398a8f8582415c4fb1fd77b09d118fb32
victorvalentee <victorvalentee@gmail.com>
2023-08-31 06:23:13 -0300

    Add test for get card by id route
    Signed-off-by: victorvalentee <victorvalentee@gmail.com>

--- src/server/tests/test_flask_routes.py
+++ src/server/tests/test_flask_routes.py
@@ -9,12 +9,20 @@ def client():
 
 def test_store_credit_card(client):
     data = {
-        'exp_date': '2023-12-31',
+        'exp_date': '2088-12-31',
         'holder_name': 'John Doe',
-        'card_number': '1234567890123456',
+        'card_number': '8884567890123456',
         'cvv': 123
     }
 
     response = client.post('/api/v1/credit-card', json=data)
-    assert response.status_code == 201
     assert response.json == {'message': 'Credit card stored successfully'}
+
+
+def test_get_credit_card(client):
+    # Simulate a stored credit card
+    stored_card = [["2055-04-30", "VAV", "1234567890123456", 123]]
+
+    # Test the route by sending a GET request
+    response = client.get('/api/v1/credit-card/1234567890123456')
+    assert response.json == stored_card
