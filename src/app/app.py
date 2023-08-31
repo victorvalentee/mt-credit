82f2b88cb693fb318bef2498c706eb73c9a3e836
victorvalentee <victorvalentee@gmail.com>
2023-08-31 05:45:57 -0300

    add route to create new credit card
    Signed-off-by: victorvalentee <victorvalentee@gmail.com>

--- src/app/app.py
+++ src/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from utils import initialize_database
 import api_functions
 import sqlite3
@@ -20,6 +20,16 @@ def get_credit_card(card_number):
     with sqlite3.connect('../database/credit_cards.db') as db_conn:
         credit_card = jsonify(api_functions.get_card_by_id(db_conn, id=card_number))
     return credit_card or jsonify({'message': 'Credit card not found'}), 404
+
+
+# Route to create new credit card
+@app.route('/api/v1/credit-card', methods=['POST'])
+def store_credit_card():
+    data = request.json
+
+    with sqlite3.connect('../database/credit_cards.db') as db_conn:
+        api_functions.create_credit_card(db_conn, credit_card_info=dict(data))
+    return jsonify({'message': 'Credit card stored successfully'}), 201
 
 
 if __name__ == '__main__':
